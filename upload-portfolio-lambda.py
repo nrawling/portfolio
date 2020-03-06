7d6ae298f3e5424e63bf397a81fc3752fd80dcbb
Nathan Rawling <nathan.rawling@hobsons.com>
2020-03-06 11:04:06 -0500

    Fixed for Python 3.6, also tweak to lamdba_handler

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,21 +1,24 @@
 import boto3
 from botocore.client import Config
-import StringIO
+import io
 import zipfile
 import mimetypes
 
-s3=boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('www.sorrowmonkey.com')
-build_bucket = s3.Bucket('build.sorrowmonkey.com')
-
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
+    s3=boto3.resource('s3', config=Config(signature_version='s3v4'))
+    
+    portfolio_bucket = s3.Bucket('www.sorrowmonkey.com')
+    build_bucket = s3.Bucket('build.sorrowmonkey.com')
+    
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+    return 'Portfolio Updated!'
