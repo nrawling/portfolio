e9bb0290a96addee4aa7d30ccaffb1050ad57c5f
Nathan Rawling <nathan.rawling@hobsons.com>
2020-03-06 11:23:14 -0500

    Added SNS notification

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,8 +5,10 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:121769289400:PortfolioPublish')
 
-    s3=boto3.resource('s3', config=Config(signature_version='s3v4'))
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     
     portfolio_bucket = s3.Bucket('www.sorrowmonkey.com')
     build_bucket = s3.Bucket('build.sorrowmonkey.com')
@@ -20,5 +22,7 @@ def lambda_handler(event, context):
             portfolio_bucket.upload_fileobj(obj, nm,
               ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
+
+    topic.publish(Subject="PortfolioDeploy", Message="Portfolio deployed successfully!")    
+
     return 'Portfolio Updated!'
